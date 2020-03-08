f90db72a723519327ae0177b6fbbdcc9674b2c53
Danil-Malkin-NN <53613490+Danil-Malkin-NN@users.noreply.github.com>
2020-03-08 23:05:32 +0300

    add score, finish game!

--- Game.js
+++ Game.js
@@ -6,6 +6,8 @@ const STEP_SIZE = 120; // px
 
 let state = PROG;
 
+let scor = 0;
+
 var Command = function(x, y, angle) {
     this.x = x;
     this.y = y;
@@ -16,8 +18,10 @@ Command.prototype.x = 0;
 Command.prototype.y = 0;
 Command.prototype.angle = 0;
 
-const weidth = 1024;
-const height = 766;
+const weidth = 1200;
+const height = 840;
+const finishX =  STEP_SIZE*7,
+		finishY = STEP_SIZE*5;
 
 let tr_top = 0;
 let tr_left = 0;
@@ -30,13 +34,15 @@ function imagePos(X , Y, angle){
 	im.style.top = (tr_top + "px");
 	im.style.left = (tr_left + "px");
 	cleanGrass(angle);
-    
+	score();
+    finishGame();
 }
 
 let commands = [];
 
 function stackCommandAdd(x, y, angle){
-    commands.push(new Command(x, y, angle));
+	if(state == PROG)
+    	commands.push(new Command(x, y, angle));
     //console.log(commands);
 }
 
@@ -57,6 +63,7 @@ function moveUp() {
 }
 
 let comandN;
+
 
 function comandStart(){
 	if (commands.length == 0)
@@ -103,68 +110,13 @@ function comandStart(){
     }
 }
 
+//Очистка команд
 function clearS(){
     commands = [];
     
 }
- sizeSugX = 108;
- sizeSugY = 80;
 
-
-window.onload = function() {
-    /*
-	let sugrob = new Image();
-	sugrob.onload = function() {
-		let canvas = document.getElementById('sugrob');
-		let context = canvas.getContext("2d");
-		context.drawImage(sugrob, sizeSugX, sizeSugY, sizeSugX, sizeSugY);
-		context.drawImage(sugrob, sizeSugX*2, sizeSugY*2, sizeSugX, sizeSugY);
-		context.drawImage(sugrob, sizeSugX*2, sizeSugY*3, sizeSugX, sizeSugY);
-		context.drawImage(sugrob, sizeSugX*2, sizeSugY*4, sizeSugX, sizeSugY);
-		context.drawImage(sugrob, sizeSugX*2, sizeSugY*5, sizeSugX, sizeSugY);
-		context.drawImage(sugrob, sizeSugX*2, sizeSugY*6, sizeSugX, sizeSugY);
-	}
-	sugrob.src = 'sugrob.png';
-	*/
-	let snow = new Image();
-	snow.onload = function(){
-		let canvas = document.getElementById('snow');
-		let context = canvas.getContext("2d");
-		context.drawImage(snow, 0, 0, 1024, 766);
-	}
-	snow.src = '1.png';	 
-	
-	//добавить тут замлю!
-	let grass = new Image();
-	grass.onload = function(){
-	    let canvas = document.getElementById('grass');
-	    let context = canvas.getContext("2d");
-	    context.drawImage(grass, 0, 0, 1024, 766);
-	}
-	grass.src = "grass.png";
-	
-	let canvas = document.getElementById('grid');
-	let context = canvas.getContext("2d");
-	
-		context.beginPath();
-	    context.lineWidth = 2;
-	    context.strokeStyle = 'red';
-	    for(let i = 0; i < height; i+= STEP_SIZE){
-	        context.moveTo(0, i);
-	        context.lineTo(weidth, i);
-	    
-	    }
-	    
-	    for(let i = 0; i < weidth; i+= STEP_SIZE){
-	        context.moveTo(i, 0);
-	        context.lineTo(i,height);
-	    
-	    }
-	    
-	    
-	    context.stroke();
-}
-
+//функция очистки земли
 function cleanGrass(angle){
     let canvas = document.getElementById('snow');
 	let context = canvas.getContext("2d");
@@ -177,6 +129,74 @@ function cleanGrass(angle){
     }else{
         context.clearRect(tr_left, tr_top, STEP_SIZE,50);
     }
+}
+function score(){
+	let doc = document.getElementById("score").innerText = scor++;
+
+}
+function finishGame(){
+	if( tr_left == finishX && tr_top == finishY){
+		alert("Конец игры, ваш счёт : " + scor + " !!")
+
+	}
+
+}
+
+//Отрисовка Линий, снега, земли, сугробов, домов наверное тоже!
+window.onload = function() {
+
+	let sugrob = new Image();
+	let finish = new Image();
+	sugrob.onload = function() {
+		let canvas = document.getElementById('sugrob');
+		let context = canvas.getContext("2d");
+		context.drawImage(sugrob, STEP_SIZE*2, STEP_SIZE*3, STEP_SIZE, STEP_SIZE);
+		context.drawImage(sugrob, STEP_SIZE*2, STEP_SIZE*4, STEP_SIZE, STEP_SIZE);
+		context.drawImage(finish, finishX, finishY, STEP_SIZE, STEP_SIZE);
+
+	}
+	finish.src = 'Finish.png';
+	sugrob.src = 'sugrob.png';
+
+
+
+	let snow = new Image();
+	snow.onload = function(){
+		let canvas = document.getElementById('snow');
+		let context = canvas.getContext("2d");
+		context.drawImage(snow, 0, 0, weidth, height);
+	}
+	snow.src = '1.png';
+
+	//добавить тут замлю!
+	let grass = new Image();
+	grass.onload = function(){
+		let canvas = document.getElementById('grass');
+		let context = canvas.getContext("2d");
+		context.drawImage(grass, 0, 0, weidth, height);
+	}
+	grass.src = "grass.png";
+
+	let canvas = document.getElementById('grid');
+	let context = canvas.getContext("2d");
+
+	context.beginPath();
+	context.lineWidth = 2;
+	context.strokeStyle = 'red';
+	for(let i = 0; i < height; i+= STEP_SIZE){
+		context.moveTo(0, i);
+		context.lineTo(weidth, i);
+
+	}
+
+	for(let i = 0; i < weidth; i+= STEP_SIZE){
+		context.moveTo(i, 0);
+		context.lineTo(i,height);
+
+	}
+
+
+	context.stroke();
 }
 
 
